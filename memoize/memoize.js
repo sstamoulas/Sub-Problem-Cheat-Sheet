a849bd8119cd2b21074c07b402d67ca82b0ab1f3
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-08-17 14:26:21 +0300

    Create memoize.js

---
+++ memoize/memoize.js
@@ -0,0 +1,33 @@
+//memoization is great way to reduce the time and possibly space complexity of a recursive algorithm by caching previously stored results and accessing them faster.
+
+function memoize(fn){
+  const cache = {}
+  return function(...args){
+    if (cache[args]){
+      return cache[args];
+    }
+    newCall = fn.apply(null, args);
+    cache[args] = newCall;
+    return newCall;
+  }
+}
+
+// Example of usage: 
+
+function getNthFib(n) {
+  // Write your code here.
+	if(n == 1) {
+		return 0
+	}
+	else if(n == 2) {
+		return 1
+	}
+	else {
+		return fastFib(n - 1) + fastFib(n - 2) // call fastFib recursively
+	}
+}
+
+const fastFib = memoize(getNthFib); // pass getNthFib as a param
+
+// Do not edit the line below.
+exports.getNthFib = getNthFib;
